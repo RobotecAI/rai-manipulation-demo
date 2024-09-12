a3eccc7eac2119c9af15615c70e2f142ec5e8e01
Magdalena Kotynia <magdalena.kotynia@robotec.ai>
2024-09-12 18:37:23 +0200

    feat: added subscription to gripper state

--- ros2_ws/src/robotic_manipulation/src/state_controller.cpp
+++ ros2_ws/src/robotic_manipulation/src/state_controller.cpp
@@ -24,8 +24,12 @@ void StateController::Begin(ArmController &arm) {
       std::make_tuple(current_pose[0], current_pose[1], current_pose[2],
                       current_pose[3], current_pose[4], current_pose[5]);
 
+  auto current_gripper_state = arm.GetGripper();
+
   RCLCPP_INFO(logger, "Current pose: %f %f %f %f %f %f", current_x, current_y,
               current_z, current_rx, current_ry, current_rz);
+
+  RCLCPP_INFO(logger, "Current gripper state: %d", current_gripper_state);
 
   auto state_subscription =
       m_node->create_subscription<std_msgs::msg::Float32MultiArray>(
@@ -40,15 +44,15 @@ void StateController::Begin(ArmController &arm) {
             double dx = msg->data[0];
             double dy = msg->data[1];
             double dz = msg->data[2];
-
             double drx = msg->data[3];
             // double dry = msg->data[4];
             // double drz = msg->data[5];
+            double gripper_state = msg->data[4];
 
             current_x = dx;
             current_y = dy;
             current_z = dz;
-
+            current_gripper_state = gripper_state;;
             current_rx = drx;
             // current_ry = dry;
             // current_rz = drz;
@@ -56,11 +60,13 @@ void StateController::Begin(ArmController &arm) {
             arm.MoveThroughWaypoints({arm.CalculatePose(
                 current_x, current_y, current_z, current_rz)});
 
-            if (msg->data[6] >= 0.5) {
+            if (msg->data[4] == 1.0) {
               arm.Open();
             } else {
               arm.Close();
             }
+
+            arm.MoveThroughWaypoints({arm.CalculatePose(0.3, 0.0, 0.35)});
           });
 
   std::this_thread::sleep_for(std::chrono::milliseconds(300000));

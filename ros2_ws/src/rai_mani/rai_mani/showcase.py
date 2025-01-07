33cfeaf507b08d8b65825ca972c3bc626a49ea3d
Kacper Dąbrowski <kacper.dabrowski@robotec.ai>
2025-01-07 17:48:31 +0100

    Add ReplaceTypes scenario
    Signed-off-by: Kacper Dąbrowski <kacper.dabrowski@robotec.ai>

--- ros2_ws/src/rai_mani/rai_mani/showcase.py
+++ ros2_ws/src/rai_mani/rai_mani/showcase.py
@@ -4,13 +4,14 @@ from rai_mani.manager import ScenarioManager
 
 from rai_mani.scenarios.move_to_the_left import MoveToTheLeftAuto
 from rai_mani.scenarios.place_on_top import PlaceOnTopAuto
+from rai_mani.scenarios.replace_types import ReplaceTypesAuto
 
 from rclpy.executors import MultiThreadedExecutor
 
 def main(args=None):
     rclpy.init(args=args)
 
-    manager = ScenarioManager([MoveToTheLeftAuto, PlaceOnTopAuto])
+    manager = ScenarioManager([ReplaceTypesAuto, MoveToTheLeftAuto, PlaceOnTopAuto])
 
     executor = MultiThreadedExecutor(2)
     executor.add_node(manager)
